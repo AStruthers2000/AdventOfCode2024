0f8ab1816dbeb06dcd7faa219dfe7def6b3fc55f
AStruthers2000 <struthers919@gmail.com>
2024-12-09 22:41:31 -0800

    Solved day 10. Classic BFS

--- Solution/Problems/Day10/Day10.h
+++ Solution/Problems/Day10/Day10.h
@@ -1,11 +1,13 @@
 ﻿#pragma once
+#include <map>
+
 #include "../Problem.h"
 
 class Day10 : public Problem
 {
 public:
     Day10(const std::string_view& filename)
-        : Problem(filename, std::nullopt)
+        : Problem(filename, "Year 2024 Day 10: Hoof It")
     {
     }
 
@@ -13,6 +15,18 @@ protected:
     void LoadProblem() override;
     std::optional<uint64_t> SolvePart1() override;
     std::optional<uint64_t> SolvePart2() override;
-    
+
+
+private:
+    struct TrailheadEvaluation
+    {
+        int score;
+        int rank;
+    };
+    std::vector<std::vector<int>> grid;
+    std::map<std::pair<int, int>, TrailheadEvaluation> trailheads;
+
+    int bfs_find_score(std::pair<int, int> start);
+    int bfs_find_rank(std::pair<int, int> start);
 };
 
